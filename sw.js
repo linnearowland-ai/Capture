770bb9d4ce5b1c63935ae77e004dbce1e5a3743f
linnearowland-ai <linnearowland@gmail.com>
2026-05-02 09:05:55 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE = 'capture-v1';
+const FILES = ['/', '/index.html'];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(caches.open(CACHE).then(function(cache) { return cache.addAll(FILES); }));
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(caches.match(e.request).then(function(r) { return r || fetch(e.request); }));
+});
